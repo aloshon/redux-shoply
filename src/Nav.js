b75a870b69ad447e927f283c955224d708e56fc6
Allen Loshon <allenlosho913@gmail.com>
2024-09-13 22:27:37 -0700

    fix readability

--- src/Nav.js
+++ src/Nav.js
@@ -11,7 +11,17 @@ function Nav() {
       <nav className="navbar">
         <NavLink exact to="/" className="shoply-home">Shoply</NavLink>
         <NavLink exact to="/cart" className="cart-stats" data-testid="cart">
-        <span className="direction"><span className="shopping-cart"><svg color="#EEE" xmlns="http://www.w3.org/2000/svg" id="Outline" viewBox="0 0 24 24" width="24" height="24"><path fill="#EEEEEE" d="M22.713,4.077A2.993,2.993,0,0,0,20.41,3H4.242L4.2,2.649A3,3,0,0,0,1.222,0H1A1,1,0,0,0,1,2h.222a1,1,0,0,1,.993.883l1.376,11.7A5,5,0,0,0,8.557,19H19a1,1,0,0,0,0-2H8.557a3,3,0,0,1-2.82-2h11.92a5,5,0,0,0,4.921-4.113l.785-4.354A2.994,2.994,0,0,0,22.713,4.077ZM21.4,6.178l-.786,4.354A3,3,0,0,1,17.657,13H5.419L4.478,5H20.41A1,1,0,0,1,21.4,6.178Z"/><circle fill="#EEEEEE" cx="7" cy="22" r="2"/><circle fill="#EEEEEE" cx="17" cy="22" r="2"/></svg><span className="quantity">{quantity}</span></span> <br/><h4 className="total">Total: ${total} <br/> Checkout</h4></span>
+        <span className="direction">
+          <span className="shopping-cart">
+            <svg color="#EEE" xmlns="http://www.w3.org/2000/svg" id="Outline" viewBox="0 0 24 24" width="24" height="24"><path fill="#EEEEEE" d="M22.713,4.077A2.993,2.993,0,0,0,20.41,3H4.242L4.2,2.649A3,3,0,0,0,1.222,0H1A1,1,0,0,0,1,2h.222a1,1,0,0,1,.993.883l1.376,11.7A5,5,0,0,0,8.557,19H19a1,1,0,0,0,0-2H8.557a3,3,0,0,1-2.82-2h11.92a5,5,0,0,0,4.921-4.113l.785-4.354A2.994,2.994,0,0,0,22.713,4.077ZM21.4,6.178l-.786,4.354A3,3,0,0,1,17.657,13H5.419L4.478,5H20.41A1,1,0,0,1,21.4,6.178Z"/>
+              <circle fill="#EEEEEE" cx="7" cy="22" r="2"/>
+              <circle fill="#EEEEEE" cx="17" cy="22" r="2"/>
+            </svg>
+            <span className="quantity">{quantity}</span>
+          </span> 
+          <br/>
+          <h4 className="total">Total: ${total} <br/> Checkout</h4>
+        </span>
         </NavLink>
 
       </nav>
